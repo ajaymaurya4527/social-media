0180c5c91bcd6e64974fb85ba0f0576613cffd0a
ajaymaurya4527 <ajay1725maurya@gmail.com>
2026-06-02 16:21:35 +0530

    multer config for production

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,14 +1,19 @@
+import fs from "fs";
 import multer from "multer";
 
-//this is configure of multer
+const uploadDir = "public/temp";
 
-const storage=multer.diskStorage({
-    destination:function (req,file,cb){
-        cb(null,"./public/temp")
-    },
-    filename:function (req,file,cb){
-        cb(null,file.originalname)
-    }
-})
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
-export const upload=multer({storage});
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+export const upload = multer({ storage });
