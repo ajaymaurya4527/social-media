af226f7918d41f14749e29566cc25b8666d576a6
ajaymaurya4527 <ajay1725maurya@gmail.com>
2026-06-02 16:23:55 +0530

    multer config

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -11,6 +11,7 @@ const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, uploadDir);
   },
+  
   filename(req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
